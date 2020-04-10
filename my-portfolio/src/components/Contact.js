11054ae8d19bce5d9163b6dabbb3aa2dac4440f6
kingmoc <joe.c.a.garcia@gmail.com>
2020-04-10 12:47:01 -0500

    connect netlify forms

--- my-portfolio/src/components/Contact.js
+++ my-portfolio/src/components/Contact.js
@@ -1,7 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Message, Icon, Header, Segment, Form, TextArea, Button } from 'semantic-ui-react';
 
 const Contact = () => {
+
+    const [contactInfo, setContactInfo] = useState({
+        name: '',
+        email: '',
+        company: '',
+        message: '',
+    })
+
+    const encode = (data) => {
+        return Object.keys(data)
+            .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+            .join("&");
+      }
+
+    const handleChange = (e, {value, name}) => {
+
+        setContactInfo({
+            ...contactInfo,
+            [name]: value
+        })
+    }
+
+    const handleSubmit = e => {
+        console.log(contactInfo)
+
+        fetch("/", {
+            method: "POST",
+            headers: { "Content-Type": "application/x-www-form-urlencoded" },
+            body: encode({ "form-name": "contact", ...contactInfo })
+          })
+            .then(() => alert("Success! - Your message has been sent, Thanks 😀"))
+            .catch(error => alert(error));
+    
+          e.preventDefault();
+          setContactInfo({        
+            name: '',
+            email: '',
+            company: '',
+            message: ''
+        })
+    }
+
     return (
         <>
             <Message icon size='big' floating className='message-contact' color='black' as='a' name='contact'>
@@ -13,30 +55,45 @@ const Contact = () => {
                 </div>
             </Message>
             <Segment size='large' raised>
-                <Form 
+                <Form
+                    name='contact' 
                     className='contact-form' 
                     size='big'
-                    method="POST" 
-                    data-netlify="true"
+                    netlify 
+                    netlify-honeypot="bot-field"
+                    onSubmit={handleSubmit} 
                 >
                     <Form.Input
                         required 
                         label='Name'
+                        name='name'
                         placeholder='Enter Name'
+                        onChange={handleChange}
+                        value={contactInfo.name}
                     />
                     <Form.Input
                         required 
                         label='Email'
+                        name='email'
+                        type='email'
                         placeholder='xyz@example.com'
+                        onChange={handleChange}
+                        value={contactInfo.email}
                     />
                     <Form.Input 
                         label='Company'
+                        name='company'
                         placeholder='Enter Company'
+                        onChange={handleChange}
+                        value={contactInfo.company}
                     />
                     <TextArea
-                        required 
+                        required
+                        name='message' 
                         style={{ minHeight: 150 }}
                         placeholder='Type message here ...'
+                        onChange={handleChange}
+                        value={contactInfo.message}
                     />
                     <Button 
                         size='huge' 
